4c1058839ab25220949502e30d75d890e6556c0b
bomin-1206 <gbm4655@naver.com>
2022-05-11 13:44:46 +0900

    Java-0511

---
+++ Java2_1/src/kr/hs/dgsw/java/detp1/d0511/homework/CommandLs.java
@@ -0,0 +1,17 @@
+package kr.hs.dgsw.java.detp1.d0511.homework;
+
+import java.io.File;
+
+public class CommandLs extends AbstractCommand {
+	
+	public CommandLs(File currentDirectory, String commandLine) {
+		super(currentDirectory, commandLine);
+	}
+
+	@Override
+	public File executeCommand() {
+		// TODO
+		return null;
+	}
+
+}
