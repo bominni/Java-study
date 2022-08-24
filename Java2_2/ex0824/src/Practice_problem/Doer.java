5a64ea441ffe6464d1a6f0f22ec2294c3d162a6a
bomin-1206 <gbm4655@naver.com>
2022-08-24 14:39:57 +0900

    Java2_2 - 0824

---
+++ Java2_2/ex0824/src/Practice_problem/Doer.java
@@ -0,0 +1,5 @@
+package Practice_problem;
+
+public interface Doer {
+    public void doJob(String name);
+}
---
+++ Java2_2/ex0824/src/Practice_problem/SayHelloDoer.java
@@ -0,0 +1,15 @@
+package Practice_problem;
+
+public class SayHelloDoer implements Doer {
+
+    @Override
+    public void doJob(String name) {
+        System.out.printf("안녕하세요. %s님\n", name);
+    }
+
+    public static void main(String[] args) {
+        MainJob job = new MainJob();
+        Doer doer = new SayHelloDoer();
+        job.execute(doer);
+    }
+}
