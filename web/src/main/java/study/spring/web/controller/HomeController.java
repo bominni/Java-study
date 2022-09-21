4271ab5cd8d10ac115780c8284d63a8cd32b0b81
bomin-1206 <gbm4655@naver.com>
2022-09-21 18:13:31 +0900

    Spring-0921

--- web/src/main/java/study/spring/web/controller/HomeController.java
+++ web/src/main/java/study/spring/web/controller/HomeController.java
@@ -5,6 +5,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import study.spring.web.domain.Person;
+import sun.java2d.pipe.SpanShapeRenderer;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @Controller
 public class HomeController {
@@ -19,5 +25,22 @@ public class HomeController {
     @RequestMapping(value = {"/hi", "/abcd", "/dgsw.do"})
     public String hi() {
         return "hi";
+    }
+
+    @RequestMapping("/clock")
+    public ModelAndView clock() {
+        ModelAndView mv = new ModelAndView("clock");
+
+        SimpleDateFormat format = new SimpleDateFormat("yyyy년 MM월 dd일 HH:mm:ss");
+        String time = format.format(new Date());
+        mv.addObject("now", time);
+
+        Person person = new Person();
+        person.setName("강보민");
+        person.setAge(18);
+
+        mv.addObject("person", person);
+
+        return mv;
     }
 }
