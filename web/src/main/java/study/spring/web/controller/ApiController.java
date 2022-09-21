5017a5976a4df24d4cb4e417f9a1ab4623679349
bomin-1206 <gbm4655@naver.com>
2022-09-21 12:17:38 +0900

    Spring-0921

---
+++ web/src/main/java/study/spring/web/controller/ApiController.java
@@ -0,0 +1,30 @@
+package study.spring.web.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import study.spring.web.domain.Person;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@RestController
+@RequestMapping("/api")
+public class ApiController {
+
+    @GetMapping("/now")
+    public String now() {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd HH:mm:ss");
+        String now = dateFormat.format(new Date());
+        return now;
+    }
+
+    @GetMapping("/person")
+    public Person person() {
+        Person person = new Person();
+        person.setName("강보민");
+        person.setAge((int)System.currentTimeMillis() % 90 + 10);
+
+        return person;
+    }
+}
